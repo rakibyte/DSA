7ff859d7cc97cc8682a4fa6c983bfe91ae58f86f
rakibyte <rakibmarvel123@gmail.com>
2025-08-02 09:20:00 +0530

    Delete old solution - Time: 0 ms (100%), Space: 13 MB (90.26%) - LeetArchive

--- LeetCode/0001-two-sum/0001-two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        hash_map = {}
-        for i, v in enumerate(nums):
-            if target - v in hash_map:
-                return i, hash_map[target - v]
-            else:
-                hash_map[v] = i
