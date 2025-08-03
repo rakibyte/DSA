5ace75497159b7d82b6fe9a6da422dbeb74dc3ed
rakibyte <rakibmarvel123@gmail.com>
2025-08-03 23:59:01 +0530

    Delete old solution - Time: 0 ms (100%), Space: 12.5 MB (57.88%) - LeetArchive

--- LeetCode/1395-minimum-time-visiting-all-points/1395-minimum-time-visiting-all-points.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def minTimeToVisitAllPoints(self, points):
-        time=0
-        for i in range(len(points)-1):
-            x1,y1=points[i]
-            x2,y2=points[i+1]
-            time_travel=max(abs(x2-x1),abs(y2-y1))
-            time+=time_travel
-        return time
