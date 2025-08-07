1927c99b26129c441327eb3fe065e0e56f20c730
rakibyte <rakibmarvel123@gmail.com>
2025-08-07 21:30:19 +0530

    Time: 123 ms (27.04%), Space: 18.9 MB (90.38%) - LeetArchive

---
+++ LeetCode/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        l,r=0,1
+        maxP=0
+        while r!=len(prices):
+            if prices[l]<prices[r]:
+                prof = prices[r]-prices[l]
+                maxP=max(maxP,prof)
+            else:
+                l=r
+            r+=1
+        return maxP
+
+        
